cc9c62c28bd50df39d1b0d83bc65d9b3e1492d02
Kawin Chinpong <kawin7538@hotmail.com>
2021-02-09 10:13:12 +0700

    add C to activity in html form

--- app/handlers.py
+++ app/handlers.py
@@ -1,16 +1,23 @@
 from typing import List
 
-from fastapi import FastAPI, Depends, HTTPException, Request, Response
+from fastapi import FastAPI, Depends, HTTPException, Request, Response, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+# from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 import logging
 
 from . import models, schemas, crud
 from .database import SessionLocal, engine
+# from .schemas import as_form
 
 models.Base.metadata.create_all(bind=engine)
 
 app=FastAPI(title="Activity Time Collector")
+
+templates = Jinja2Templates(directory='templates')
 
 @app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
@@ -31,14 +38,23 @@ def get_db():
         db.close()
 
 @app.get("/")
-def main():
+async def main():
     return "This is the first page"
 
-@app.post("/activities/", response_model=schemas.Activity)
-def create_activity(activity: schemas.ActivityCreate, db: Session = Depends(get_db)):
-    return crud.create_activity(db=db, activity=activity)
+@app.post("/add_activity", response_model=schemas.Activity)
+async def create_activity(request: Request, activity_id: int = Form(...), activity_name: str = Form(...), activity_time: float = Form(...), db: Session = Depends(get_db)):
+    # print(activity_name,activity_time)
+    print(activity_id,activity_name,activity_time)
+    activity=schemas.ActivityCreate(activity_id=activity_id,activity_name=activity_name,activity_time=activity_time)
+    crud.create_activity(db=db, activity=activity)
+    return RedirectResponse("/activities",status_code=303)
 
-@app.get("/activities/", response_model=List[schemas.Activity])
-def read_activities(db: Session = Depends(get_db)):
+@app.get("/activities", response_model=List[schemas.Activity], response_class=HTMLResponse)
+async def read_activities(request: Request,db: Session = Depends(get_db)):
     activities = crud.get_activities(db)
-    return activities
+    data={
+        'activities':activities,
+        'tid':5,
+    }
+    # return activities
+    return templates.TemplateResponse('activities.html',{"request": request,'data':data})
--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,57 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from pydantic.fields import ModelField
 
-from typing import Optional
+from fastapi import Form
+
+from typing import Optional, Type
+
+import inspect
+
+# def as_form(cls: Type[BaseModel]):
+#     new_parameters = []
+
+#     for field_name, model_field in cls.__fields__.items():
+#         model_field: ModelField  # type: ignore
+
+#         if not model_field.required:
+#             new_parameters.append(
+#                 inspect.Parameter(
+#                     model_field.alias,
+#                     inspect.Parameter.POSITIONAL_ONLY,
+#                     default=Form(model_field.default),
+#                     annotation=model_field.outer_type_,
+#                 )
+#             )
+#         else:
+#             new_parameters.append(
+#                 inspect.Parameter(
+#                     model_field.alias,
+#                     inspect.Parameter.POSITIONAL_ONLY,
+#                     default=Form(...),
+#                     annotation=model_field.outer_type_,
+#                 )
+#             )
+
+#     async def as_form_func(**data):
+#         return cls(**data)
+
+#     sig = inspect.signature(as_form_func)
+#     sig = sig.replace(parameters=new_parameters)
+#     as_form_func.__signature__ = sig  # type: ignore
+#     setattr(cls, 'as_form', as_form_func)
+#     return cls
 
 class ActivityBase(BaseModel):
     activity_id : int
     activity_name : str
     activity_time : Optional[float] = 0
 
-class ActivityCreate(ActivityBase):
-    pass
-
-class Activity(ActivityBase):
     class Config:
         orm_mode=True
+
+class ActivityCreate(ActivityBase):
+    activity_name : str
+    activity_time : float
+
+class Activity(ActivityBase):
+    pass
