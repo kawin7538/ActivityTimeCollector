0c1cd7762d513527d69c868f60023b1d48bbb820
Kawin Chinpong <kawin7538@hotmail.com>
2021-03-10 19:20:52 +0700

    update port something

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from app.handlers import app
 from ml_models import *
+import os
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app,host='0.0.0.0',port=5000,debug=True)
+    port=int(os.environ.get('PORT',5000))
+    uvicorn.run(app,host='0.0.0.0',port=port,debug=True)
